6916132179b0a0c3c172f6acb9dbd12d153adb06
atulyaaj <atulyaaj6@gmail.com>
2025-06-29 19:59:50 +0530

    API added

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 async function getGreeting() {
   const name = document.getElementById("nameInput").value;
   const res = await fetch(
-    `https://your-api.vercel.app/api/hello?name=${encodeURIComponent(name)}`
+    `https://greeting-site-henna.vercel.app/api/hello?name=${encodeURIComponent(
+      name
+    )}`
   );
   const data = await res.json();
   document.getElementById("result").innerText = data.message;
